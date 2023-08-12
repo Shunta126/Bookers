7594d86d4461bbf7e21ca97b98b3cbfcede80548
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-08-12 10:08:09 +0000

    14まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,25 @@
 class BooksController < ApplicationController
 
-  def top
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
-  def new
-  end
 
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+  params.require(:book).permit(:title, :body)
   end
 end
