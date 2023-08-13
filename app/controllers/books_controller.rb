63d66461cb38ce7f6d885bbb94c1fc26b67629f1
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-08-13 15:23:27 +0000

    20まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,29 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
 
-
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path(@book.id)
   end
 
   private
